4ab60d7d75583323615705c73c0d08c32334342c
Justin McLaren <justinmclaren@Justins-MacBook-Pro.local>
2018-03-05 13:19:54 -0800

    config gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
-    author: 'Kyle Mathews',
-    description: 'A starter blog demonstrating what Gatsby can do.',
-    siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
+    title: 'Siyu Capital',
+    author: 'Justin McLaren',
+    description: 'Blog for Siyu Capital',
+    siteUrl: 'https://github.com/chinapalace/siyu-blog',
   },
   pathPrefix: '/gatsby-starter-blog',
   plugins: [
